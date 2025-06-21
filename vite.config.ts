b216909b1fa0c20d10cf11b34f4ffcfad9261191
Esteban Araoz <tu-correo@ejemplo.com>
2025-06-21 14:12:54 -0300

    habilitacion visualizacion externa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: true, // Muy importante: escucha en todas las interfaces
+    allowedHosts: ['earaoz.dyndns.org'],
+  }
 })
