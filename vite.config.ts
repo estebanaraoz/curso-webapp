260a70195c564ff48e2a6506dac98610bd486c63
estebanaraoz <araozesteban@gmail.com>
2025-07-07 01:14:46 -0300

    chore: set base path for subdirectory

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/curso-webapp/',
   server: {
     host: true, // Muy importante: escucha en todas las interfaces
-    allowedHosts: ['earaoz.dyndns.org'],
+    allowedHosts: ['earaoz.dyndns.org', 'localhost'],
   },
 })
+
